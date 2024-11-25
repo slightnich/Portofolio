897f33936d2d71970ac4efb58cad58329ed67818
Slight Kens <kensbotz@gmail.com>
2024-11-26 05:49:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // router
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pubilc/views', 'index.html'));
+  res.sendFile(path.join(__dirname, 'pubilc', 'index.html'));
 });
 
 app.get('/version', async (req, res) => {
